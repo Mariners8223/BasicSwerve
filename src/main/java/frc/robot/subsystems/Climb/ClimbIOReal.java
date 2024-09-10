2a7fbc8bf405439c787225e1c08ab95b6c30c44e
FrcIroniH <54024395+FrcIroniH@users.noreply.github.com>
2024-09-10 16:24:54 +0300

    fix previous incorrect commit - Mika

--- src/main/java/frc/robot/subsystems/Climb/ClimbIOReal.java
+++ src/main/java/frc/robot/subsystems/Climb/ClimbIOReal.java
@@ -4,5 +4,56 @@
 
 package frc.robot.subsystems.Climb;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 /** Add your docs here. */
-public class ClimbIOReal {}
+public class ClimbIOReal implements ClimbIO {
+
+    TalonFX motorFx;
+    public ClimbIOReal(TalonFX motorFx){this.motorFx = motorFx;}
+
+    @Override
+    public void setBrake(){motorFx.setNeutralMode(NeutralModeValue.Brake);}
+
+    @Override
+    public void setCoast(){motorFx.setNeutralMode(NeutralModeValue.Coast);
+    }
+    //TODO: Ask Eyal if this is correct...? (the brake and set)(also: ConfigStatorCurrentLimit to slow the brake?)
+
+    @Override
+    public void setMotorDutyCycle(double power){motorFx.set(power);}
+
+    @Override
+    public void resetEncoder(){motorFx.setPosition(0);}
+
+    @Override
+    public void setMaximum(){
+        SoftwareLimitSwitchConfigs config = new SoftwareLimitSwitchConfigs();
+        config.ForwardSoftLimitThreshold = 0;
+        config.ForwardSoftLimitEnable = true;
+        motorFx.getConfigurator().apply(config);
+    }
+
+    @Override
+    public void setMinimum(){
+        SoftwareLimitSwitchConfigs config = new SoftwareLimitSwitchConfigs();
+        config.ForwardSoftLimitThreshold = 0;
+        config.ForwardSoftLimitEnable = true;
+        motorFx.getConfigurator().apply(config);
+    }
+
+    public void update(ClimbInputAutoLogged inputs){
+        inputs.motorCurrent = motorFx.getSupplyCurrent().getValueAsDouble() ; 
+        inputs.motorPosition = motorFx.getPosition().getValueAsDouble();
+        inputs.motorSpeed=  motorFx.getVelocity().getValueAsDouble();
+        inputs.motorTemperature = motorFx.getDeviceTemp().getValueAsDouble();
+    }
+
+
+}
