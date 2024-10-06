b8af90923961809fa7289efa64d17cde12d41921
captainsoccer <justeyallevi@gmail.com>
2024-10-06 14:00:30 +0300

    added periodic and more

--- src/main/java/frc/util/RoborioController.java
+++ src/main/java/frc/util/RoborioController.java
@@ -1,6 +1,7 @@
 package frc.util;
 
 import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import java.util.ArrayList;
 
@@ -8,16 +9,28 @@ public abstract class RoborioController {
 
     private static final double DEFAULT_PERIOD = 0.02;
 
-    private static final ArrayList<Runnable> runnables = new ArrayList<>();
+    private static final ArrayList<RoborioController> motors = new ArrayList<>();
 
-    protected static final Notifier notifier = new Notifier(() -> runnables.forEach(Runnable::run));
+    protected static final Notifier notifier = new Notifier(() -> motors.forEach(RoborioController::run));
 
-    protected void addRunnable(Runnable runnable) {
-        runnables.add(runnable);
+    protected void addRunnable(RoborioController motor) {
+        motors.add(motor);
     }
+
+    protected abstract void run();
+
+    private static final SubsystemBase subsystem = new SubsystemBase() {
+        @Override
+        public void periodic() {
+            motors.forEach(RoborioController::update);
+        }
+    };
+
+    protected abstract void update();
 
     public static void start() {
         notifier.startPeriodic(DEFAULT_PERIOD);
+        subsystem.setName("RoborioControllers");
     }
 
 }
--- src/main/java/frc/util/RoborioSpark.java
+++ src/main/java/frc/util/RoborioSpark.java
@@ -1,33 +1,161 @@
 package frc.util;
 
-import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkAbsoluteEncoder;
+import com.revrobotics.SparkRelativeEncoder;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 public class RoborioSpark extends RoborioController {
     private final CANSparkMax spark;
-    ProfiledPIDController pidController;
+    private final ProfiledPIDController[] controllers = new ProfiledPIDController[3];
+    private final Double[] feedforward = new Double[3];
 
+    private final AtomicReference<Double>[] setpoints = new AtomicReference[3];
 
+    private int currentController = 0;
+
+    private Supplier<Double> referenceDevice;
+
+    private double maxOutput = 12;
+    private double minOutput = -12;
 
 
     public RoborioSpark(int id, CANSparkLowLevel.MotorType type) {
         spark = new CANSparkMax(0, type);
+        spark.restoreFactoryDefaults();
 
+        for (int i = 0; i < controllers.length; i++) {
+            controllers[i] = new ProfiledPIDController(0, 0, 0, new TrapezoidProfile.Constraints(0, 0));
+        }
 
+        Arrays.fill(feedforward, 0.0);
 
+        for (int i = 0; i < setpoints.length; i++) {
+            setpoints[i] = new AtomicReference<Double>(0.0);
+        }
 
-        addRunnable(this::run);
+        var encoder = spark.getEncoder();
+        referenceDevice = encoder::getPosition;
+
+        addRunnable(this);
     }
 
     public CANSparkMax getSpark() {
         return spark;
     }
 
+    private void checkIndex(int index){
+        if(index < 0 || index >= controllers.length)
+            throw new IndexOutOfBoundsException("controller " + index + " does not exist");
+    }
 
-    private void run(){
-        spark.set(0.5);
+    public void setMaxOutput(double maxOutput){
+        this.maxOutput = maxOutput;
+    }
+
+    public void setMinOutput(double minOutput){
+        this.minOutput = minOutput;
+    }
+
+    public void setReferenceDevice(Supplier<Double> referenceDevice){
+        this.referenceDevice = referenceDevice;
+    }
+
+    public void setReferenceDevice(SparkRelativeEncoder referenceDevice){
+        this.referenceDevice = referenceDevice::getPosition;
+    }
+
+    public void setReferenceDevice(SparkAbsoluteEncoder referenceDevice){
+        this.referenceDevice = referenceDevice::getPosition;
+    }
+
+    public ProfiledPIDController getPIDController(){
+        return controllers[0];
+    }
+
+    public ProfiledPIDController getPIDController(int index){
+        checkIndex(index);
+
+        return controllers[index];
+    }
+
+    public void setPIDController(ProfiledPIDController controller){
+        controllers[0] = controller;
+    }
+
+    public void setPIDController(ProfiledPIDController controller, int index){
+        checkIndex(index);
+
+        controllers[index] = controller;
+    }
+
+    public void setKp(double kp){
+        controllers[0].setP(kp);
+    }
+
+    public void setKp(double kp, int index){
+        checkIndex(index);
+
+        controllers[index].setP(kp);
+    }
+
+    public void setKi(double ki){
+        controllers[0].setI(ki);
+    }
+
+    public void setKi(double ki, int index){
+        checkIndex(index);
+
+        controllers[index].setI(ki);
+    }
+
+    public void setKd(double kd){
+        controllers[0].setD(kd);
+    }
+
+    public void setKd(double kd, int index){
+        checkIndex(index);
+
+        controllers[index].setD(kd);
+    }
+
+    public void setKf(double feedforward){
+        this.feedforward[0] = feedforward;
+    }
+
+    public void setKf(double feedforward, int index){
+        checkIndex(index);
+
+        this.feedforward[index] = feedforward;
+    }
+
+
+    @Override
+    protected void run(){
+        ProfiledPIDController controller = controllers[currentController];
+
+        double setpoint = setpoints[currentController].get();
+
+        double reference = referenceDevice.get();
+
+        double output = controller.calculate(reference, setpoint);
+
+        output += feedforward[currentController] * setpoint;
+
+        output = Math.min(maxOutput, Math.max(minOutput, output));
+
+        spark.setVoltage(output);
+    }
+
+    @Override
+    protected void update(){
+        currentController = (currentController + 1) % controllers.length;
     }
 }
