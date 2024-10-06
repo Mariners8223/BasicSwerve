7d6e4adc421f0fd78ca0e2e5c565fc494909078d
captainsoccer <justeyallevi@gmail.com>
2024-10-06 09:03:51 +0300

    added PID contoller

--- src/main/java/frc/util/RoborioSpark.java
+++ src/main/java/frc/util/RoborioSpark.java
@@ -3,15 +3,19 @@ package frc.util;
 import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
 public class RoborioSpark extends RoborioController {
     private final CANSparkMax spark;
+    ProfiledPIDController pidController;
 
 
 
 
     public RoborioSpark(int id, CANSparkLowLevel.MotorType type) {
         spark = new CANSparkMax(0, type);
+
 
 
 
